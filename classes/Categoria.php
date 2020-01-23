014fd97fa36e361c6312b70191ebb9776017a9f2
Marcelo Júnior <rokermarcelo@gmail.com>
2020-01-23 08:14:47 -0300

    Funcionalidade de editar Categoria

--- classes/Categoria.php
+++ classes/Categoria.php
@@ -6,6 +6,13 @@ class Categoria {
 
     public $id;
     public $nome;
+    
+    public function __construct($id = false) {
+        if($id){
+            $this->id = $id;
+            $this->carregar();
+        }
+    }
 
     public function listar() {
         $query = "SELECT id, nome FROM categorias";
@@ -15,8 +22,24 @@ class Categoria {
         return $lista;
     }
 
+    public function carregar() {
+        $query = "SELECT id, nome FROM categorias WHERE id={$this->id}";
+        $conexao = Conexao::getConexao();
+        $resultado = $conexao->query($query);
+        $lista = $resultado->fetchAll();
+        foreach ($lista as $linha) {
+            $this->nome = $linha['nome'];
+        }
+    }
+    
     public function inserir() {
         $query = "INSERT INTO categorias (nome) VALUES ('{$this->nome}')";
+        $conexao = Conexao::getConexao();
+        $conexao->exec($query);
+    }
+    
+    public function atualizar() {
+        $query = "UPDATE categorias SET nome='{$this->nome}' WHERE id={$this->id}";
         $conexao = Conexao::getConexao();
         $conexao->exec($query);
     }
