d0a33a1cce359106bc13e8d8582160258d5d015a
Marcelo Júnior <rokermarcelo@gmail.com>
2020-01-24 14:40:50 -0300

    Funcionalidades de editar e excluir Produto

--- classes/Produto.php
+++ classes/Produto.php
@@ -7,8 +7,15 @@ class Produto {
     public $preco;
     public $quantidade;
     public $categoria_id;
-    
-    public static function listar(){
+
+    function __construct($id = false) {
+        if ($id) {
+            $this->id = $id;
+            $this->carrregar();
+        }
+    }
+
+    public static function listar() {
         $query = "SELECT p.id, p.nome, preco, quantidade, categoria_id, c.nome as categoria "
                 . "FROM produtos p INNER JOIN categorias c "
                 . "ON p.categoria_id=c.id "
@@ -18,19 +25,56 @@ class Produto {
         $lista = $resultado->fetchAll();
         return $lista;
     }
-    
-    public function inserir(){
+
+    public function carrregar() {
+        $query = "SELECT nome, preco, quantidade, categoria_id FROM produtos "
+                . "WHERE id=:id";
+        $conexao = Conexao::getConexao();
+        $stmt = $conexao->prepare($query);
+        $stmt->bindValue(":id", $this->id);
+        $stmt->execute();
+        $linha = $stmt->fetch();
+        $this->nome = $linha['nome'];
+        $this->preco = $linha['preco'];
+        $this->quantidade = $linha['quantidade'];
+        $this->categoria_id = $linha['categoria_id'];
+    }
+
+    public function inserir() {
 //        $query = "INSERT INTO produtos (nome, preco, quantidade, categoria_id) "
 //                . "VALUES ('{$this->nome}', {$this->preco}, {$this->quantidade}, {$this->categoria_id});";
 //        echo "$query";
         $query = "INSERT INTO produtos (nome, preco, quantidade, categoria_id) "
                 . "VALUES (:nome, :preco, :quantidade, :categoria_id);";
-                $conexao = Conexao::getConexao();
-                $stmt = $conexao->prepare($query);
-                $stmt->bindValue(':nome', $this->nome);
-                $stmt->bindValue(':preco', $this->preco);
-                $stmt->bindValue(':quantidade', $this->quantidade);
-                $stmt->bindValue(':categoria_id', $this->categoria_id);
-                $stmt->execute();
+        $conexao = Conexao::getConexao();
+        $stmt = $conexao->prepare($query);
+        $stmt->bindValue(':nome', $this->nome);
+        $stmt->bindValue(':preco', $this->preco);
+        $stmt->bindValue(':quantidade', $this->quantidade);
+        $stmt->bindValue(':categoria_id', $this->categoria_id);
+        $stmt->execute();
+    }
+
+    public function atualizar() {
+        $query = "UPDATE produtos SET "
+                . "nome=:nome, preco=:preco, "
+                . "quantidade=:quantidade, categoria_id=:categoria_id "
+                . "WHERE id=:id";
+        $conexao = Conexao::getConexao();
+        $stmt = $conexao->prepare($query);
+        $stmt->bindValue(':id', $this->id);
+        $stmt->bindValue(':nome', $this->nome);
+        $stmt->bindValue(':preco', $this->preco);
+        $stmt->bindValue(':quantidade', $this->quantidade);
+        $stmt->bindValue(':categoria_id', $this->categoria_id);
+        $stmt->execute();
+    }
+
+    public function excluir() {
+        $query = "DELETE FROM produtos WHERE id=:id";
+        $conexao = Conexao::getConexao();
+        $stmt = $conexao->prepare($query);
+        $stmt->bindValue(":id", $this->id);
+        $stmt->execute();
     }
 }
